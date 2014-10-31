57e8e82c6cea04e4faccb28c618897d17ff23ad2
Foad Mozaffari <mozafff@modahealth.com>
2014-10-31 15:02:49 -0700

    country detail view

--- app/library.js
+++ app/library.js
@@ -3,6 +3,8 @@
  */
 angular.module('App')
     .constant('URL_PATH', 'http://api.geonames.org/countryInfoJSON?username=foadm_2000')
+    .constant('COUNTRY_PATH', 'http://api.geonames.org/countryInfoJSON?username=foadm_2000&country=')
+    .constant('CAPITAL_PATH', 'http://api.geonames.org/countryInfoJSON?username=mwhelan&country=')
     .factory("CountryInfo", function($http, URL_PATH){
         var request = {
             callback: 'JSON_CALLBACK'
@@ -10,13 +12,36 @@ angular.module('App')
         return function(){
             return $http({
                 method: 'JSONP',
-                url: URL_PATH,
+                url: URL_PATH, 
                 params: request
             })
         }
-        .factory("countryDetail", function($http,$routeParams, $scope){
-                $scope.country_code = $routeParams.country;
-                return function(){console.log($scope.country_code);}
-
+    })
+    .factory("countryDetail", function($route,$http, COUNTRY_PATH){
+        var country_code = $route.current.params.country;
+        var country_path = COUNTRY_PATH + country_code;
+        var request = {
+            callback: 'JSON_CALLBACK'
+        };
+        return function(){
+            return $http({
+                method: 'JSONP',
+                url: country_path,
+                params: request
             })
+        }
+    })
+    .factory("capitalDetail", function($route,$http, CAPITAL_PATH) {
+        var country_code = $route.current.params.country;
+        var capital_path = CAPITAL_PATH + country_code;
+        var request = {
+            callback: 'JSON_CALLBACK'
+        };
+        return function () {
+            return $http({
+                method: 'JSONP',
+                url: capital_path,
+                params: request
+            })
+        }
     })
--- app/views/detail/detail.js
+++ app/views/detail/detail.js
@@ -2,6 +2,20 @@
  * Created by mozafff on 10/29/2014.
  */
 angular.module('App')
-    .controller('DetailCtrl', function($scope, $routeParams){
-        $scope.country_code = $routeParams.country;
+    .controller('DetailCtrl', function($scope, countryDetail,capitalDetail){
+        $scope.getCountry = function(){ // I need to refactor this into a resolve function in the routes.js
+            countryDetail().
+                success(function(data){
+                    $scope.result = data;
+                })
+        }
+        $scope.getCapital = function(){ // I need to refactor this into a resolve function in the routes.js
+            capitalDetail().
+                success(function(data){
+                    $scope.capResult = data;
+                    console.log($scope.capResult);
+                })
+        }
+        $scope.getCountry();
+        $scope.getCapital();
     })
